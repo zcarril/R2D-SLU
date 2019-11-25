e14b2b3f977d0d0df7532c36385d13a3abfa3d6f
zcarril <zachary.carril@slu.edu>
2019-11-24 22:11:18 -0600

    adding angles
    added angle capturing and modified the motor file to using in the main

--- dcmotors.py
+++ dcmotors.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
+#initializing motors
 in1 = 24
 in2 = 23
 en = 25
@@ -16,7 +17,7 @@ GPIO.setup(en,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
 p=GPIO.PWM(en,1000)
-
+temp1=1
 #secondmotor
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in3,GPIO.OUT)
@@ -25,85 +26,27 @@ GPIO.setup(en2,GPIO.OUT)
 GPIO.output(in3,GPIO.LOW)
 GPIO.output(in4,GPIO.LOW)
 p2=GPIO.PWM(en2,1000)
-
-p.start(25)
-p2.start(25)
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
-
-while(1):
-
-    x=raw_input()
-    
-    if x=='r':
-        print("run")
-        if(temp1==1):
-         GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in2,GPIO.LOW)
-         GPIO.output(in3,GPIO.HIGH)
-         GPIO.output(in4,GPIO.LOW)
-         print("forward")
-         x='z'
-        else:
-         GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
-         GPIO.output(in3,GPIO.LOW)
-         GPIO.output(in4,GPIO.HIGH)
-         print("backward")
-         x='z'
+pTemp = 80
+pTemp2 = 80
+#initial duty cycle
+p.start(pTemp)
+p2.start(pTemp)
 
 
-    elif x=='s':
-        print("stop")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
-        x='z'
+   
+def straight():
+    GPIO.output(in1,GPIO.HIGH)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.HIGH)
+    GPIO.output(in4,GPIO.LOW)
 
-    elif x=='f':
-        print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in3,GPIO.HIGH)
-        GPIO.output(in4,GPIO.LOW)
-        temp1=1
-        x='z'
+#this needs to tell the motors what to do
+#given triggers from the left, front, and right list
+def decide(leftList,frontList,rightList):
+#     print("left: "+ str(leftList))
+#     print("front: " +  str(frontList))
+#     print("right: " + str(rightList))
+    print("this does nothing rn")
 
-    elif x=='b':
-        print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.HIGH)
-        temp1=0
-        x='z'
-
-    elif x=='l':
-        print("low")
-        p.ChangeDutyCycle(25)
-        p2.ChangeDutyCycle(25)
-        x='z'
-
-    elif x=='m':
-        print("medium")
-        p.ChangeDutyCycle(50)
-        p2.ChangeDutyCycle(50)
-        x='z'
-
-    elif x=='h':
-        print("high")
-        p.ChangeDutyCycle(75)
-        p2.ChangeDutyCycle(75)
-        x='z'
-     
-    
-    elif x=='e':
-        GPIO.cleanup()
-        break
-    
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+def clean():
+    GPIO.cleanup()
--- lidarAvgAngle.py
+++ lidarAvgAngle.py
@@ -1,42 +1,69 @@
+#python file that controls the motors
+import dcmotors
+#for math stuff
+import math
 #average angle program
 #lidar imports and setup
 from rplidar import RPLidar
 lidar = RPLidar('/dev/ttyUSB0')
+#for threading(if needed)
 import threading
 
-distanceConst = 400
+distanceConst = 500
 
 def avgList(avg, *args):
     if avg[0] != []:
         avgL = avg[0]
-        dist = avg[1]
+        dist = 0
         for i in range(len(avgL)):
             dist += avgL[i]
         dist = dist/(len(avgL))
         return dist
 
+def getThresh(ang, *args):
+    if ang[1] != []:
+        base = 0
+        dist = distanceConst
+        angle = ang[1]
+        midAngle = angle[int(len(angle)/2)]
+        if abs(midAngle-90) < 45:
+            base = 90
+        elif abs(midAngle-180) < 45:
+            base = 180
+        elif abs(midAngle-270) < 45:
+            base = 270
+        midAngle = abs(base - midAngle)
+        threshold = dist/math.cos(math.radians(midAngle))
+        return threshold
+        
 
+def printSame(num, check, limit):
+    x=0
+    dConstMedium = limit
+    dConstClose = limit-300
+    if check == False:
+        if num < dConstClose:
+            x=2
+            print("# ", end=" ")
+        elif num < dConstMedium:
+            x=1
+            print("* ", end=" ")
+        else:
+            print("  ", end=" ")
+    if check == True:
+        if num < dConstClose:
+            x=2
+            print("#")
+        elif num < dConstMedium:
+            x=1
+            print("*")
+        else:
+            print(" ")
+    return x
 
-def printSame(num, check):
-    dConstMedium = 800
-    dConstClose = 400
-    try:
-        if check == False:
-            if num < dConstClose:
-                print("# ", end=" ")
-            elif num < dConstMedium:
-                print("* ", end=" ")
-            else:
-                print("  ", end=" ")
-        if check == True:
-            if num < dConstClose:
-                print("#")
-            elif num < dConstMedium:
-                print("*")
-            else:
-                print(" ")
-    except TypeError:
-        print("big oof")
+        
+        
+    
 
 
 try:
@@ -54,152 +81,198 @@ try:
             rAvg1,rAvg2,rAvg3,rAvg4,rAvg5,rAvg6,rAvg7,rAvg8,rAvg9,rAvg10 = (0 for i in range(10))
             
             #TODO need to have list for angles
+            fAng1,fAng2,fAng3,fAng4,fAng5,fAng6,fAng7,fAng8,fAng9,fAng10 = ([] for i in range(10))
+            lAng1,lAng2,lAng3,lAng4,lAng5,lAng6,lAng7,lAng8,lAng9,lAng10 = ([] for i in range(10))
+            rAng1,rAng2,rAng3,rAng4,rAng5,rAng6,rAng7,rAng8,rAng9,rAng10 = ([] for i in range(10))
             
-         
+            #new list of triggers to motors
+            mListLeft,mListFront,mListRight=([] for i in range(3))
+            
             for (_, angle, distance) in scan:
                 #Left
                 #L1
                 if angle > 45  and angle <= 54:
                     lList1.append(distance)
+                    lAng1.append(angle)
                 #L2
                 if angle > 54 and angle <= 63:
                     lList2.append(distance)
+                    lAng2.append(angle)
                 #F3
                 if angle > 63 and angle <= 72:
                     lList3.append(distance)
+                    lAng3.append(angle)
                 #F4
                 if angle > 72 and angle <= 81:
                     lList4.append(distance)
+                    lAng4.append(angle)
                 #F5
                 if angle > 81 and angle <= 90:
                     lList5.append(distance)
+                    lAng5.append(angle)
                 #F6
                 if angle > 90 and angle <= 99:
                     lList6.append(distance)
+                    lAng6.append(angle)
                 #F7
                 if angle > 99 and angle <= 108:
                     lList7.append(distance)
+                    lAng7.append(angle)
                 #F8
                 if angle > 108 and angle <= 117:
                     lList8.append(distance)
+                    lAng8.append(angle)
                 #F9
                 if angle > 117 and angle <= 126:
                     lList9.append(distance)
+                    lAng9.append(angle)
                 #F10
                 if angle > 126 and angle <= 135:
                     lList10.append(distance)
+                    lAng10.append(angle)
                 
                 #Front
                 #F1
                 if angle > 135 and angle <= 144:
                     fList1.append(distance)
+                    fAng1.append(angle)
                 #F2
                 if angle > 144 and angle <= 153:
                     fList2.append(distance)
+                    fAng2.append(angle)
                 #F3
                 if angle > 153 and angle <= 162:
                     fList3.append(distance)
+                    fAng3.append(angle)
                 #F4
                 if angle > 162 and angle <= 171:
                     fList4.append(distance)
+                    fAng4.append(angle)
                 #F5
                 if angle > 171 and angle <= 180:
                     fList5.append(distance)
+                    fAng5.append(angle)
                 #F6
                 if angle > 180 and angle <= 189:
                     fList6.append(distance)
+                    fAng6.append(angle)
                 #F7
                 if angle > 189 and angle <= 198:
                     fList7.append(distance)
+                    fAng7.append(angle)
                 #F8
                 if angle > 198 and angle <= 207:
                     fList8.append(distance)
+                    fAng8.append(angle)
                 #F9
                 if angle > 207 and angle <= 216:
                     fList9.append(distance)
+                    fAng9.append(angle)
                 #F10
                 if angle > 216 and angle <= 225:
                     fList10.append(distance)
+                    fAng10.append(angle)
                 
                 #RIGHT
                 #R1
                 if angle > 225 and angle <= 234:
                     rList1.append(distance)
+                    rAng1.append(angle)
                 #R2
                 if angle > 234 and angle <= 243:
                     rList2.append(distance)
+                    rAng2.append(angle)
                 #R3
                 if angle > 243 and angle <= 252:
                     rList3.append(distance)
+                    rAng3.append(angle)
                 #R4
                 if angle > 252 and angle <= 261:
                     rList4.append(distance)
+                    rAng4.append(angle)
                 #R5
                 if angle > 261 and angle <= 270:
                     rList5.append(distance)
+                    rAng5.append(angle)
                 #R6
                 if angle > 270 and angle <= 279:
                     rList6.append(distance)
+                    rAng6.append(angle)
                 #R7
                 if angle > 279 and angle <= 288:
                     rList7.append(distance)
+                    rAng7.append(angle)
                 #R8
                 if angle > 288 and angle <= 297:
                     rList8.append(distance)
+                    rAng8.append(angle)
                 #R9
                 if angle > 297 and angle <= 306:
                     rList9.append(distance)
+                    rAng9.append(angle)
                 #R10
                 if angle > 306 and angle <= 315:
                     rList10.append(distance)
+                    rAng10.append(angle)
                     
                     
             #creating distance list and (empty) average variable touples
             #TODO: put list of angles instead of "lAvg1"
-            main.extend(((lList1,lAvg1),
-                         (lList2,lAvg2),
-                         (lList3,lAvg3),
-                         (lList4,lAvg4),
-                         (lList5,lAvg5),
-                         (lList6,lAvg6),
-                         (lList7,lAvg7),
-                         (lList8,lAvg8),
-                         (lList9,lAvg9),
-                         (lList10,lAvg10),
-                         (fList1,fAvg1),
-                         (fList2,fAvg2),
-                         (fList3,fAvg3),
-                         (fList4,fAvg4),
-                         (fList5,fAvg5),
-                         (fList6,fAvg6),
-                         (fList7,fAvg7),
-                         (fList8,fAvg8),
-                         (fList9,fAvg9),
-                         (fList10,fAvg10),
-                         (rList1,rAvg1),
-                         (rList2,rAvg2),
-                         (rList3,rAvg3),
-                         (rList4,rAvg4),
-                         (rList5,rAvg5),
-                         (rList6,rAvg6),
-                         (rList7,rAvg7),
-                         (rList8,rAvg8),
-                         (rList9,rAvg9),
-                         (rList10,rAvg10)))
+            main.extend(((lList1,lAng1),
+                         (lList2,lAng2),
+                         (lList3,lAng3),
+                         (lList4,lAng4),
+                         (lList5,lAng5),
+                         (lList6,lAng6),
+                         (lList7,lAng7),
+                         (lList8,lAng8),
+                         (lList9,lAng9),
+                         (lList10,lAng10),
+                         (fList1,fAng1),
+                         (fList2,fAng2),
+                         (fList3,fAng3),
+                         (fList4,fAng4),
+                         (fList5,fAng5),
+                         (fList6,fAng6),
+                         (fList7,fAng7),
+                         (fList8,fAng8),
+                         (fList9,fAng9),
+                         (fList10,fAng10),
+                         (rList1,rAng1),
+                         (rList2,rAng2),
+                         (rList3,rAng3),
+                         (rList4,rAng4),
+                         (rList5,rAng5),
+                         (rList6,rAng6),
+                         (rList7,rAng7),
+                         (rList8,rAng8),
+                         (rList9,rAng9),
+                         (rList10,rAng10)))
             
             #iterating through list of touples
-            for i in range(len(main)):
+            for i in range(len(main)):  
                 avgDist = avgList(main[i])
+                thresh = getThresh(main[i])
+                
                 endLine = False
                 if avgDist != None:
                     if i < len(main)-1:
-                        printSame(avgDist,endLine)
+                        if i < 10:
+                            mListLeft.append(printSame(avgDist,endLine,thresh))
+                        elif i < 20:
+                            mListFront.append(printSame(avgDist,endLine,thresh))
+                        elif i < 30:
+                            mListRight.append(printSame(avgDist,endLine,thresh))
                     else:
                         endLine = True
-                        printSame(avgDist,endLine)
-
+                        mListRight.append(printSame(avgDist,endLine,thresh))
+            #TODO: designate what the motors should given anything.
+            #the function below will do that
+            #dcmotors.decide(mListLeft,mListFront,mListRight)
+            dcmotors.straight()
 except KeyboardInterrupt:
     lidar.stop()
     lidar.stop_motor()
     lidar.disconnect()
+    dcmotors.clean()
     print("YOU DIED")
