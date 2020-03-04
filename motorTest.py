0bb2fcba96e30af2fcff01f3e11a12f0921d271b
zcarril <zachary.carril@slu.edu>
2020-03-04 14:07:52 -0600

    new motor test program
    trying this with a new driver with larger motors. using PWM in a different way than the L298N. The new driver is a Sabertooth 2X12.

---
+++ motorTest.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+GPIO.cleanup()
+
+m1 = 24
+m2 = 23
+
+#switches == 1 1 0 0 0 0 ( mode, LiMode, Mixed, exponential, sensitivity )
+#to lookup pinout -> "pinout" in terminal
+#GPIO 24 = pin 18 --> go to M1 on driver
+#GPIO 23 = pin 16 --> go to M2 on driver
+#ground pins 6, 14, 20  --> go to 0V on driver
+#5V pins 2 & 4 --> go to 5V on driver
+
+
+GPIO.setmode(GPIO.BOARD)   # Numbers pins by physical location
+GPIO.setup(m1, GPIO.OUT)   # Set pin mode as output
+GPIO.output(m1, GPIO.LOW)  # Set pin to low(0V)
+GPIO.setup(m2, GPIO.OUT)   # Set pin mode as output
+GPIO.output(m2, GPIO.LOW)  # Set pin to low(0V)
+
+p = GPIO.PWM(m1, 1000)     # set Frequece to 1KHz
+p2 = GPIO.PWM(m2, 1000)    # set Frequece to 1KHz
+p.start(50)                # Start PWM output, Duty Cycle = 50
+p2.start(50)
+
+
+# so ~0 Duty should be full reverse, whereas ~100 Duty should be full forward, ~50 Duty should be stopped
+
+try:
+        print('start')
+        while True:
+                for dc in range(100, -1, -10):    # Decrease duty cycle: 100~0
+                        p.ChangeDutyCycle(dc)     # Change duty cycle
+                        p2.ChangeDutyCycle(dc)
+                        print('Duty Cycle = ', dc)
+                        time.sleep(5)
+        p.stop()
+        GPIO.output(m1, GPIO.HIGH)    # turn off all leds
+             
+except KeyboardInterrupt:
+        p.stop()
+        GPIO.output(LedPin, GPIO.HIGH)    # turn off all leds
+        p2.stop()
+        GPIO.output(m2, GPIO.HIGH)    # turn off all leds
+        GPIO.cleanup()
